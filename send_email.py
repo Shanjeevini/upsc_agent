d59830a6679aab74138b74e0a7411b2fc5990f66
Shanjeevini <shanjeevini.senthil@gmail.com>
2026-03-05 11:15:36 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,30 +1,38 @@
 import smtplib
-from email.message import EmailMessage
 import os
+from email.message import EmailMessage
 
 def send_email():
 
-    email_user = os.getenv("EMAIL_USER")
-    email_pass = os.getenv("EMAIL_PASS")
-    email_to = os.getenv("EMAIL_TO")
+    EMAIL_USER = os.environ["EMAIL_USER"]
+    EMAIL_PASS = os.environ["EMAIL_PASS"]
+    EMAIL_TO = os.environ["EMAIL_TO"]
 
     msg = EmailMessage()
-    msg["Subject"] = "UPSC Daily Current Affairs"
-    msg["From"] = email_user
-    msg["To"] = email_to
+    msg["Subject"] = "UPSC Daily Current Affairs Report"
+    msg["From"] = EMAIL_USER
+    msg["To"] = EMAIL_TO
 
-    msg.set_content("Attached is today's UPSC current affairs report.")
+    msg.set_content("Attached is today's UPSC Daily Current Affairs PDF report.")
 
+    # attach PDF
     with open("upsc_daily_report.pdf", "rb") as f:
         file_data = f.read()
 
-    msg.add_attachment(file_data,
-                       maintype="application",
-                       subtype="pdf",
-                       filename="upsc_daily_report.pdf")
+    msg.add_attachment(
+        file_data,
+        maintype="application",
+        subtype="pdf",
+        filename="upsc_daily_report.pdf"
+    )
+
+    print("Connecting to Gmail SMTP...")
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-        smtp.login(email_user, email_pass)
+        smtp.login(EMAIL_USER, EMAIL_PASS)
         smtp.send_message(msg)
 
-    print("Email sent successfully")
+    print("Email sent successfully!")
+
+if __name__ == "__main__":
+    send_email()
